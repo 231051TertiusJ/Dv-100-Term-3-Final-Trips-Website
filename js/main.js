db7473c97b1e4c8565f5dd2241eb810b2a6eced1
231051TertiusJ <125471734+231051TertiusJ@users.noreply.github.com>
2023-08-09 22:09:00 +0200

    add style and start java

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+$(document).ready(function(){
+
+    console.log("Toets");
+
+    // ---------------------------------------
+    // Trips load page
+    loadTrips();
+
+})
+
+function loadTrips(){
+
+    for (let i= 0; i <loadTrips.length; i++) {
+        console.log(trips)
+    }
+
+
+}
